1fa2934bb5cc29f52ce7d9ff9f9da51c0310c8d7
Mihaela Čalogović <mihaela.calogovic@gmail.com>
2023-06-01 14:20:51 +0200

    Implemented the main camera controller and making prefabs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float minHeight, maxHeight;
+
+    public Transform farBackground, middleBackground;
+    private Vector2 lastPos;
+
+    // Start is called before the first frame update
+    void Start() {
+        lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Camera following a specific target setup in Unity
+        transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z);
+
+        // Making the far background move at the same speed as the camera and making the 
+        // middle background move a little slower so that we get the effect of a dynamic background
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y);
+        farBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f);
+        middleBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f) * .5f;
+        lastPos = transform.position;
+    }
+}
